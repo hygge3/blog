ad08441cf49c44546a33fc85be3c27366827632a
hygge <zhanglh30@gmail.com>
2023-05-22 23:06:03 +0800

    添加css模块

--- source/scripts/injector.js
+++ source/scripts/injector.js
@@ -1,4 +1,4 @@
 hexo.extend.filter.register('theme_inject', function(injects) {
   injects.header.file('bodyBegin', 'source/js/timeliness.js');
-  injects.footer.raw('bodyEnd', '<script async src="https://cdn.jsdelivr.net/gh/fz6m/Private-web@1.2/js/sakura/sakura-small.js" crossorigin="anonymous"></script>');
+  injects.footer.raw('bodyEnd', '<script src="https://cdn.jsdelivr.net/gh/fz6m/Private-web@1.2/js/sakura/sakura-small.js"></script>');
 });
