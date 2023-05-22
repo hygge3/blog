7dc0162a8b621a3a734d8752c62c7e5045b84620
hygge <zhanglh30@gmail.com>
2023-05-22 15:12:19 +0800

    添加注入器

---
+++ source/scripts/injector.js
@@ -0,0 +1,10 @@
+hexo.extend.injector.register(
+  "body_end",
+  '<script src="/js/timeliness.js"></script>',
+  "post"
+);
+hexo.extend.injector.register(
+  "body_end",
+  '<script src="/js/sakura.js"></script>',
+  "default"
+);
